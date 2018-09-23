2e74aaaec9e249f06d81ea24da35e03630e135c5
Stefan Wagner <stw@bit-strickerei.de>
2018-09-23 15:14:38 +0200

    Add editor event tests

--- test/cljs/tales/doo_runner.cljs
+++ test/cljs/tales/doo_runner.cljs
@@ -3,10 +3,12 @@
             [tales.views-test]
             [tales.geometry-test]
             [tales.events.core-test]
+            [tales.events.editor-test]
             [tales.events.stage-test]))
 
 (doo-tests
   'tales.views-test
   'tales.geometry-test
   'tales.events.core-test
+  'tales.events.editor-test
   'tales.events.stage-test)
