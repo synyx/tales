23f53d7cf754c55ab7e74fd5635b173916415d7f
Stefan Wagner <stw@bit-strickerei.de>
2018-10-23 23:08:12 +0200

    Make css transform-matrix more readable

--- src/cljs/tales/util/css.cljs
+++ src/cljs/tales/util/css.cljs
@@ -3,5 +3,5 @@
 (defn transform-origin [offset-x offset-y]
   (str offset-x "px " offset-y "px"))
 
-(defn transform-matrix [a b c d tx ty]
-  (str "matrix(" a "," b "," c "," d "," tx "," ty ")"))
+(defn transform-matrix [m00 m10 m01 m11 m02 m12]
+  (str "matrix(" m00 "," m10 "," m01 "," m11 "," m02 "," m12 ")"))
