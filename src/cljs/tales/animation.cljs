974d8184001c617539dcfb87722d3c4975a35f64
Stefan Wagner <stw@bit-strickerei.de>
2018-10-30 00:00:14 +0100

    Implement "Smooth and efficient zooming and panning" animation

--- src/cljs/tales/animation.cljs
+++ src/cljs/tales/animation.cljs
@@ -1,4 +1,7 @@
-(ns tales.animation)
+(ns tales.animation
+  (:require [thi.ng.geom.core :as g]
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as m]))
 
 (defn linear
   "Linearly interpolates between `a` and `b` relative to `t` in `duration`."
@@ -18,3 +21,47 @@
   (if (fn? easing)
     (easing a b duration)
     (partial (get easings easing) a b duration)))
+
+(defn- cosh [x] (/ (+ (Math/pow Math/E x) (Math/pow Math/E (- x))) 2))
+(defn- sinh [x] (/ (- (Math/pow Math/E x) (Math/pow Math/E (- x))) 2))
+(defn- tanh [x] (/ (sinh x) (cosh x)))
+
+(defn- b [w0 w1 u1 rho i]
+  (let [w (if (= i 1) w1 w0)
+        n1 (- (Math/pow w1 2) (Math/pow w0 2))
+        n2 (* (Math/pow -1 i) (Math/pow rho 4) (Math/pow u1 2))
+        d (* 2 w u1 (Math/pow rho 2))]
+    (/ (+ n1 n2) d)))
+
+(defn- r [b]
+  (Math/log (- (Math/sqrt (+ (Math/pow b 2) 1)) b)))
+
+(defn- S [r0 r1 rho]
+  (/ (- r1 r0) rho))
+
+(defn- u [w0 r0 rho s]
+  (let [a (* w0 (cosh r0) (tanh (+ (* rho s) r0)))
+        b (* w0 (sinh r0))]
+    (/ (- a b) (Math/pow rho 2))))
+
+(defn- w [w0 r0 rho s]
+  (/ (* w0 (cosh r0)) (cosh (+ (* rho s) r0))))
+
+(defn smooth-efficient [c0 w0 c1 w1 width V rho]
+  "Implementation of \"Smooth and efficient zooming and panning\",
+  Jarke J. van Wijk and Wim A. A. Nuij, TU Eindhoven, Netherlands."
+  (let [u1 (g/dist (gv/vec2 c0) c1)
+        b0 (b w0 w1 u1 rho 0)
+        b1 (b w0 w1 u1 rho 1)
+        r0 (r b0)
+        r1 (r b1)
+        duration (* 1000 (S r0 r1 rho) V)
+        tick-fn (fn [t]
+                  (let [t (/ t 1000)
+                        s (Math/min (* V t) (S r0 r1 rho))
+                        us (u w0 r0 rho s)
+                        ws (w w0 r0 rho s)
+                        pos (m/mix (gv/vec2 c0) c1 (/ us u1))
+                        scale (/ ws width)]
+                    [pos scale]))]
+    [duration tick-fn]))
