256930dcdb491169dac3f2885df3485f1f124bae
Stefan Wagner <stw@bit-strickerei.de>
2018-11-14 22:41:31 +0100

    Automatically scroll current slide preview into view

--- src/cljs/tales/views/preview.cljs
+++ src/cljs/tales/views/preview.cljs
@@ -1,5 +1,6 @@
 (ns tales.views.preview
-  (:require [re-frame.core :refer [dispatch subscribe]]))
+  (:require [reagent.core :as r]
+            [re-frame.core :refer [dispatch subscribe]]))
 
 (defn- slide-scale [slide target-width target-height]
   "Calculates the scale factor to resize the slide to target-width/-height"
@@ -9,38 +10,48 @@
       (/ target-height slide-height)
       (/ target-width slide-width))))
 
-(defn slide [props project slide]
-  (let [rect (:rect slide)
-        preview-width (:width props)
-        preview-height (:height props)
-        active? (:active? props)
-        scale (slide-scale slide preview-width preview-height)
-        dx (* scale (:x rect))
-        dy (* scale (:y rect))
-        scaled-slide-width (* scale (:width rect))
-        scaled-slide-height (* scale (:height rect))
-        scaled-img-width (* scale (:width (:dimensions project)))
-        scaled-img-height (* scale (:height (:dimensions project)))]
-    [:div.slide-preview-list-item
-     {:style {:width preview-width :min-width preview-width
-              :height preview-height :min-height preview-height
-              :background-color "#333"
-              :border-width 3
-              :border-style "solid"
-              :border-color (if active? "#ff0000" "#333")}
-      :on-click #(dispatch [:slide/activate (:index slide)])
-      :on-double-click #(dispatch [:camera/fly-to-rect (:rect slide)])}
-     [:div
-      {:style {:width scaled-slide-width
-               :height scaled-slide-height
-               :background-color "#fff"
-               :background-repeat "no-repeat"
-               :background-image (str "url(" (:file-path project) ")")
-               :background-size (str
-                                  scaled-img-width "px "
-                                  scaled-img-height "px")
-               :background-position-x (- dx)
-               :background-position-y (- dy)}}]]))
+(defn slide []
+  (let [this (r/current-component)
+        did-update (fn []
+                     (let [props (r/props this)
+                           active? (:active? props)]
+                       (if active?
+                         (.scrollIntoView (r/dom-node this)))))
+        render (fn [props project slide]
+                 (let [rect (:rect slide)
+                       preview-width (:width props)
+                       preview-height (:height props)
+                       active? (:active? props)
+                       scale (slide-scale slide preview-width preview-height)
+                       dx (* scale (:x rect))
+                       dy (* scale (:y rect))
+                       scaled-slide-width (* scale (:width rect))
+                       scaled-slide-height (* scale (:height rect))
+                       scaled-img-width (* scale (:width (:dimensions project)))
+                       scaled-img-height (* scale (:height (:dimensions project)))]
+                   [:div.slide-preview-list-item
+                    {:style {:width preview-width :min-width preview-width
+                             :height preview-height :min-height preview-height
+                             :background-color "#333"
+                             :border-width 3
+                             :border-style "solid"
+                             :border-color (if active? "#ff0000" "#333")}
+                     :on-click #(dispatch [:slide/activate (:index slide)])
+                     :on-double-click #(dispatch [:camera/fly-to-rect (:rect slide)])}
+                    [:div
+                     {:style {:width scaled-slide-width
+                              :height scaled-slide-height
+                              :background-color "#fff"
+                              :background-repeat "no-repeat"
+                              :background-image (str "url(" (:file-path project) ")")
+                              :background-size (str
+                                                 scaled-img-width "px "
+                                                 scaled-img-height "px")
+                              :background-position-x (- dx)
+                              :background-position-y (- dy)}}]]))]
+    (r/create-class {:display-name "slide"
+                     :component-did-update did-update
+                     :reagent-render render})))
 
 (defn slides [project]
   (let [slides (subscribe [:slides])
