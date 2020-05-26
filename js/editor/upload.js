114929dd3eb201dc8d67c60ef3358ed180264990
Stefan Wagner <wagner@synyx.de>
2020-05-26 13:12:40 +0200

    Determine image dimensions before uploading

--- js/editor/upload.js
+++ js/editor/upload.js
@@ -10,9 +10,99 @@ export let uploader = tale => {
         type: "file",
       },
       on: {
-        change: ev =>
-          trigger("projects/update-image", tale, ev.target.files[0]),
+        change: ev => {
+          determineImageDimensions(ev.target.files[0])
+            .then(({ file, width, height }) => {
+              trigger("projects/update", {
+                ...tale,
+                dimensions: { width, height },
+              });
+              trigger("projects/update-image", tale, file);
+            })
+            .catch(err =>
+              console.error("error determining image dimensions", err),
+            );
+        },
       },
     }),
   ]);
 };
+
+export function determineImageDimensions(file) {
+  return new Promise((resolve, reject) => {
+    if (file.type === "image/svg+xml") {
+      loadSvg(file)
+        .then(svg => {
+          let { width, height } = parseSvgDimensions(svg);
+          if (width && height) {
+            resolve({ file, width, height });
+          } else {
+            reject("undefined width or height");
+          }
+        })
+        .catch(reject);
+    } else {
+      loadImage(file)
+        .then(img => {
+          let width = img.naturalWidth || img.width;
+          let height = img.naturalHeight || img.height;
+          if (width && height) {
+            resolve({ file, width, height });
+          } else {
+            reject("undefined width or height");
+          }
+        })
+        .catch(reject);
+    }
+  });
+}
+
+function loadSvg(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = ev => {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(ev.target.result, file.type);
+      let svgs = doc.getElementsByTagName("svg");
+      if (svgs.length > 0) {
+        resolve(svgs[0]);
+      } else {
+        reject("failed to get svg from document: " + doc);
+      }
+    };
+    reader.onerror = reject;
+    reader.readAsText(file);
+  });
+}
+
+function loadImage(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = ev => {
+      let image = new Image();
+      image.onload = () => resolve(image);
+      image.onerror = () => reject("failed to load image: " + image.src);
+      image.src = ev.target.result;
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+function parseSvgDimensions(svg) {
+  let width = parseFloat(svg.getAttribute("width"));
+  let height = parseFloat(svg.getAttribute("height"));
+  let viewbox = svg.getAttribute("viewBox");
+  if (!isNaN(width) && !isNaN(height)) {
+    return { width, height };
+  }
+
+  viewbox = viewbox.split(" ").map(parseFloat);
+  width = viewbox[2] - viewbox[0];
+  height = viewbox[3] - viewbox[1];
+  if (!isNaN(width) && !isNaN(height)) {
+    return { width, height };
+  }
+
+  return {};
+}
---
+++ js/editor/upload.test.js
@@ -0,0 +1,115 @@
+import { determineImageDimensions } from "./upload";
+
+/* global global */
+
+global.Image = class {
+  constructor() {
+    this._src = undefined;
+    Object.defineProperty(this, "width", {
+      get: () => global.Image.width,
+    });
+    Object.defineProperty(this, "height", {
+      get: () => global.Image.height,
+    });
+    Object.defineProperty(this, "src", {
+      get: () => this._src,
+      set: src => {
+        this._src = src;
+        setTimeout(this.onload, 0);
+      },
+    });
+  }
+};
+
+const pngFile = imageFromBase64(
+  "foo.png",
+  "image/png",
+  "iVBORw0KGgoAAAANSUhEUgAAAGQAAABQCAQAAABFABhkAAAAWUlEQVR42u3P" +
+    "AQ0AAAgDoL9/aK3hHDSgmbxQERERERERERERERERERERERERERERERERERER" +
+    "ERERERERERERERERERERERERERERERERERERERERERERkUsWHIBQAXlFihYA" +
+    "AAAASUVORK5CYII=",
+);
+
+let svgFile1 = svg(
+  "foo.svg",
+  '<?xml version="1.0" ?><svg width="100mm" height="80mm"></svg>',
+);
+let svgFile2 = svg(
+  "foo.svg",
+  '<?xml version="1.0" ?><svg viewBox="20 10 120 100"></svg>',
+);
+let svgFile3 = svg(
+  "foo.svg",
+  '<?xml version="1.0" ?><svg viewBox="foo bar"></svg>',
+);
+let xmlFile = svg("foo.xml", '<?xml version="1.0" ?>');
+
+beforeEach(() => {
+  global.Image.width = undefined;
+  global.Image.height = undefined;
+});
+
+describe("determineImageDimensions", () => {
+  it("returns width and height from image", () => {
+    global.Image.width = 100;
+    global.Image.height = 80;
+    expect.assertions(3);
+    return determineImageDimensions(pngFile).then(({ file, width, height }) => {
+      expect(file).toBe(pngFile);
+      expect(width).toBe(100);
+      expect(height).toBe(80);
+    });
+  });
+  it("rejects on unknown width or height", () => {
+    expect.assertions(1);
+    return determineImageDimensions(pngFile).catch(reason =>
+      expect(reason).toBe("undefined width or height"),
+    );
+  });
+  it("returns width and height from svg width and height", () => {
+    expect.assertions(3);
+    return determineImageDimensions(svgFile1).then(
+      ({ file, width, height }) => {
+        expect(file).toBe(svgFile1);
+        expect(width).toBe(100);
+        expect(height).toBe(80);
+      },
+    );
+  });
+  it("returns width and height from svg viewbox", () => {
+    expect.assertions(3);
+    return determineImageDimensions(svgFile2).then(
+      ({ file, width, height }) => {
+        expect(file).toBe(svgFile2);
+        expect(width).toBe(100);
+        expect(height).toBe(90);
+      },
+    );
+  });
+  it("rejects on invalid svg viewbox", () => {
+    expect.assertions(1);
+    return determineImageDimensions(svgFile3).catch(reason =>
+      expect(reason).toBe("undefined width or height"),
+    );
+  });
+  it("rejects on invalid svg", () => {
+    expect.assertions(1);
+    return determineImageDimensions(xmlFile).catch(reason =>
+      expect(reason).toContain("failed to get svg from document:"),
+    );
+  });
+});
+
+function imageFromBase64(name, contentType, encodedImage) {
+  const bstr = atob(encodedImage);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], name, { type: contentType });
+}
+
+function svg(name, value) {
+  return new File([value], name, { type: "image/svg+xml" });
+}
